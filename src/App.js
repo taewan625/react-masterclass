107051947baac3b165167f322a9966f2443900ae
taewan625 <taewan1997625@gmail.com>
2025-07-28 07:58:51 +0900

    #2.2. Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
+// styled-components 사용법.
 const Father = styled.div`
   display: flex;
 `;
 
 const Box = styled.div`
-  background-color: ${(props) => props.color || "black"};
+  //props로 스타일을 동적으로 변경
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: white;
+//Circle 컴포넌트는 Box 컴포넌트를 상속받아 스타일을 변경
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <Box color="teal">
-        <Text>Box 1</Text>
-      </Box>
-      <Box color="tomato"></Box>
+      <Box bgColor="teal"></Box>
+      <Circle bgColor="tomato"></Circle>
     </Father>
   );
 }
