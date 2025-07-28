af1b9b534632fad1acda07e3953b2ac01c0b001e
taewan625 <taewan1997625@gmail.com>
2025-07-29 00:34:50 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAni = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 50px;
-`;
-
-const Box = styled.div`
-  display: flex;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-  height: 100px;
-  width: 100px;
-  background-color: tomato;
-  animation: ${rotationAni} 1s linear infinite;
-  ${Emoji}:hover {
-    color: white;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+  font-size: 48px;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">123</Emoji>
-      </Box>
-      <Emoji as="p">asdf</Emoji>
+      <Title>Hello World</Title>
     </Wrapper>
   );
 }
