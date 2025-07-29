2079b2d3724d2b76a1b0bb84cad029593b2669f6
taewan625 <taewan1997625@gmail.com>
2025-07-30 01:14:49 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState<string>("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello, ", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" placeholder="username" value={value} onChange={onChange} />
-        <button>login</button>
-      </form>
-    </div>
+    <Container>
+      <H1>hello world</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { darkTheme } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
