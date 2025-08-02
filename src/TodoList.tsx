4997f9c294e621afac8b7c119020d1fa7e46587d
taewan625 <taewan1997625@gmail.com>
2025-08-02 23:34:25 +0900

    #6.6 Forms

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [todo, setValue] = useState("");
+  /*   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
 
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
+    setToDo(event.currentTarget.value);
   };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(todo);
-  };
+    if (toDo.length < 10) {
+      return setToDoError("To do should be logner");
+    }
+    setToDoError("");
+  }; */
+
+  //register: onChange, value 세팅할 필요 없음
+  //watch: 값의 변화를 보는 함수
+  const { register, watch } = useForm();
+  console.log(watch());
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          type="text"
-          placeholder="Write a to do "
-          value={todo}
-        />
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="firstName" />
+        <input {...register("lastName")} placeholder="lastName" />
+        <input {...register("username")} placeholder="username" />
+        <input {...register("password")} placeholder="password" />
+
         <button type="submit">Add</button>
       </form>
     </div>
