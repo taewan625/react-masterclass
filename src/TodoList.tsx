3e699ef1672d55dc32f1cc1ca1c17ca07c8b9d6f
taewan625 <taewan1997625@gmail.com>
2025-08-02 23:58:37 +0900

    #6.7 Form Validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  /*   const [toDo, setToDo] = useState("");
+  /*
+  const [toDo, setToDo] = useState("");
   const [toDoError, setToDoError] = useState("");
 
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -14,21 +15,48 @@ function ToDoList() {
       return setToDoError("To do should be logner");
     }
     setToDoError("");
-  }; */
+  }; 
+  */
 
   //register: onChange, value 세팅할 필요 없음
   //watch: 값의 변화를 보는 함수
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+
+  console.log(formState.errors);
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="firstName" />
-        <input {...register("lastName")} placeholder="lastName" />
-        <input {...register("username")} placeholder="username" />
-        <input {...register("password")} placeholder="password" />
+      {/* handleSubmit으로 유효성 검증을 마친 후 사용자 생성함수인 onValid를 호출 */}
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("Email", { required: "isRequired" })}
+          placeholder="Email"
+        />
+        <input
+          {...register("firstName", {
+            required: true,
+            minLength: { value: 10, message: "short over 10" },
+          })}
+          placeholder="firstName"
+        />
+        <input
+          {...register("lastName", { required: true, minLength: 5 })}
+          placeholder="lastName"
+        />
+        <input
+          {...register("username", { required: true })}
+          placeholder="username"
+        />
+        <input
+          {...register("password", { required: true })}
+          placeholder="password"
+        />
 
         <button type="submit">Add</button>
       </form>
