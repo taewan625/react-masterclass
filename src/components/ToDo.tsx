c7b4d37b911e585cc8d54d109a1f1cfd02b6d378
taewan625 <taewan1997625@gmail.com>
2025-08-03 11:06:03 +0900

    #6.15 Immutability part Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -4,18 +4,50 @@ import { IToDo, toDoState } from "../atoms";
 function ToDo({ id, text, category }: IToDo) {
   const setToDos = useSetAtom(toDoState);
 
+  const onClick = (name: IToDo["category"]) => {
+    setToDos((oldToDos) => {
+      return oldToDos.map((oldToDo: IToDo) =>
+        oldToDo.id === id ? { ...oldToDo, category: name } : oldToDo
+      );
+    });
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
+
+/* name 형식 todo category 변경 단점: 타입 체크가 불안정 As로 type 추정 강제함.
+import { useSetAtom } from "jotai";
+import { IToDo, toDoState } from "../atoms";
+
+function ToDo({ id, text, category }: IToDo) {
+  const setToDos = useSetAtom(toDoState);
+
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
 
     setToDos((oldToDos) => {
-      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
-      const newTodo = { text, id, category: name };
-
-      return oldToDos;
-      //oldToDos.map((oldToDo: IToDo) => oldToDo.id === id ? { ...oldToDo, category: name } : oldToDo
+      return oldToDos.map((oldToDo: IToDo) =>
+        oldToDo.id === id
+          ? { ...oldToDo, category: name as IToDo["category"] }
+          : oldToDo
+      );
     });
   };
 
@@ -42,3 +74,4 @@ function ToDo({ id, text, category }: IToDo) {
 }
 
 export default ToDo;
+ */
