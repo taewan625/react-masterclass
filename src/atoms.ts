a6a8fe8cdd59e67fcb07984f622dddfa57e56a4a
taewan625 <taewan1997625@gmail.com>
2025-08-05 00:54:10 +0900

    #7.0 Get Selectors

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,38 +1,14 @@
 import { atom } from "jotai";
 
-export enum SessionStorage {
-  "TODO_DATA" = "TODO_DATA",
+enum TIME {
+  HOUR = 60,
 }
 
-//type Categories = "DOING" | "TO_DO" | "DONE";
+export const minuteState = atom<number>(11);
 
-//type 보다 더 명확함. defaul value = number index
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-  "DELETE" = "DELETE",
-}
-
-export interface IToDo {
-  id: number;
-  text: string;
-  //string에서 특정 문자열만 가능하도록 제약
-  category: Categories;
-}
-
-//상태 정의
-export const toDoState = atom<IToDo[]>([]);
-
-//상태 정의
-export const categoryState = atom<Categories>(Categories.TO_DO);
-
-//atom 데이터 가공 제공방법 ps. Recoil의 selector
-export const toDoSelector = atom((get) => {
-  //toDoState 주시
-  const toDos = get(toDoState);
-  const category = get(categoryState);
-
-  //가공한 data 반환
-  return toDos.filter((todo) => todo.category === category);
+export const hourSelect = atom((get) => {
+  //주시 대상
+  const minutes = get(minuteState);
+  //반환 값
+  return minutes / TIME.HOUR;
 });
