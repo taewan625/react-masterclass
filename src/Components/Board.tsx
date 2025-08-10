8d40bb092567bfd61f3af52b783021b5396968c7
taewan625 <taewan1997625@gmail.com>
2025-08-10 12:17:23 +0900

    #7.13 Refs
    ref: dom을 직접 조작해야되는 경우 (focus, scroll, drag-drop 등)
    목적별 차이
    - useState: 값 조작
    - useRef: dom 조작
    
    그래도 ref 사용은 최대한 지양할 것

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from "@hello-pangea/dnd";
 import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   padding: 20px 10px;
@@ -42,18 +43,33 @@ interface IBoardProps {
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  //react에서 ref에 접근하기 위해서 useRef 라는 ref 객체를 만들어야 한다.
+  //useRef로 관리해야되는 ref를 react가 인지
+  const inputRef = useRef<HTMLInputElement>(null); //null은 초기값으로 ref DOM이 만들어지지 않아서 최초에는 null일수 밖에 없음
+
+  const onClick = () => {
+    // inputRef.current: 참조 주소를 가진 dom 그자체로 document.getElementById() 같이 dom을 가져오는 것
+    // 그래서 focus라는 dom 에 이벤트를 바로 줄 수 있다.
+    inputRef.current?.focus();
+  };
+
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      {/* ref: 해당 dom node의 직접적인 참조 주소 */}
+      <input ref={inputRef} type="text" placeholder="grab me" />
+      <button type="button" onClick={onClick}>
+        click me
+      </button>
 
       <Droppable droppableId={boardId}>
         {/* proviede: @hello-pangea/dnd 라이브러리가 내부에서 넘겨주는 매개변수 객체 */}
         {(provided, snapshot) => (
           <Area
-            isDraggingOver={snapshot.isDraggingOver}
-            isDraggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
             ref={provided.innerRef}
             {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
           >
             {toDos.map((todo, index) => (
               <DraggableCard
