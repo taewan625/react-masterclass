8be152957674859f59d71222dd1ce57fae01e765
taewan625 <taewan1997625@gmail.com>
2025-08-10 18:36:15 +0900

    #8.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { motion, stagger, Variants } from "motion/react";
+import { motion, Variants } from "motion/react";
+import { useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -9,9 +10,20 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 150px;
+  height: 150px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -27,16 +39,22 @@ const boxVariants: Variants = {
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      {/* 자식에게도 애니메이션이 동일 적용 됨. 기본 값 */}
-      <Box
-        drag
-        variants={boxVariants}
-        whileDrag="whileDrag"
-        whileHover="whileHover"
-        whileTap="whileTap"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag //drag 가능. =x, =y로 축 기준 이동 제약 가능
+          dragConstraints={biggerBoxRef} //이동 제약
+          dragSnapToOrigin
+          dragElastic={0} //이동 제약 기준 관성
+          variants={boxVariants}
+          whileDrag="whileDrag"
+          whileHover="whileHover"
+          whileTap="whileTap"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
