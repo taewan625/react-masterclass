b7a6c93eab257f9fa52fd41fa6afd72fb327b437
taewan625 <taewan1997625@gmail.com>
2025-08-10 17:32:33 +0900

    #8.3 Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "motion/react";
+import { motion, Variants } from "motion/react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,14 +17,21 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+//Variant: framer motion에서 애니메이션 상태들을 객체 형태로 정의할 때 사용
+const myVars: Variants = {
+  start: { opacity: 0, scale: 0 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+    transition: { type: "spring", bounce: 0.8 },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1, rotateZ: 360 }}
-        transition={{ type: "spring", bounce: 0.8 }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
       {/* 애니메이션 사용법 */}
     </Wrapper>
   );
