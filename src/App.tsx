2867ece5c92518a323571f60199d31d0b5e60cca
taewan625 <taewan1997625@gmail.com>
2025-08-10 21:14:42 +0900

    #8.8 MotionValues part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { motion, useMotionValue } from "motion/react";
+import { motion, useMotionValue, useTransform } from "motion/react";
 import { useEffect } from "react";
 import styled from "styled-components";
 
@@ -21,24 +21,12 @@ const Box = styled(motion.div)`
 function App() {
   const x = useMotionValue(0);
 
-  //useEffect: 렌더링 이후 실행되야하는 작업인 경우 사용
-  //useEffect 안에 설정된 동작이 리렌더링, 혹은 렌더링 해제가 될때 해당 이벤트를 제거 및 중복 방지를 해준다.
-  useEffect(() => {
-    //x 자체는 motion의 객체로써 변경되지 않지만 on 메서드를 통해서 x 내부 값의 변경을 읽는다.
-    //useEffect안에 넣음으로서 해당 on 동작이 렌더링 혹은 리렌더링 시, 메서드가 중복 호출되지 않게 막는다 == 메모리 누수 방지
-    x.on("change", () => console.log(x.get()));
-  }, [x]);
+  //측정 값을 가지고 원하는 출력값 출력 방법
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
 
   return (
     <Wrapper>
-      <button
-        onClick={() => {
-          x.set(200);
-        }}
-      >
-        click me
-      </button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, scale: scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
