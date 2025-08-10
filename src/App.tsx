3a625b845ff64ff519327a6286888f08b7685319
taewan625 <taewan1997625@gmail.com>
2025-08-10 18:24:44 +0900

    #8.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -10,45 +10,19 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  border-radius: 35px;
-  place-self: center; //display-grid에서 사용. align-self, justify-self 축약형
-`;
-
 const boxVariants: Variants = {
-  initial: { opacity: 0, scale: 0 },
-  animate: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      //type: "spring", //update 된 이후부터 type:spring 쓰면 제대로 동작 안함.
-      duration: 1,
-      delayChildren: stagger(0.3, { startDelay: 0.5, from: "first" }),
-    },
-  },
-};
-
-const circleVariants: Variants = {
-  initial: {
-    opacity: 0,
-    y: 10,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
+  whileHover: { scale: 1.2, rotateZ: 90 },
+  whileTap: { scale: 1, borderRadius: "100px" },
+  //rgba써야지 동적으로 색상 적용
+  whileDrag: {
+    backgroundColor: "rgb(46, 204, 113)",
   },
 };
 
@@ -56,12 +30,13 @@ function App() {
   return (
     <Wrapper>
       {/* 자식에게도 애니메이션이 동일 적용 됨. 기본 값 */}
-      <Box variants={boxVariants} initial="initial" animate="animate">
-        <Circle variants={circleVariants}></Circle>
-        <Circle variants={circleVariants}></Circle>
-        <Circle variants={circleVariants}></Circle>
-        <Circle variants={circleVariants}></Circle>
-      </Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileDrag="whileDrag"
+        whileHover="whileHover"
+        whileTap="whileTap"
+      ></Box>
     </Wrapper>
   );
 }
