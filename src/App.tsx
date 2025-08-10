5e7296628d12e028c07d43ad23fe6ebb4f1b5796
taewan625 <taewan1997625@gmail.com>
2025-08-10 17:17:31 +0900

    #8.2 Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1, rotateZ: 360 }}
+        transition={{ type: "spring", bounce: 0.8 }}
+      />
       {/* 애니메이션 사용법 */}
-      <motion.div></motion.div>
     </Wrapper>
   );
 }
