a08a71d4a3336223dd19e0f27fc9ed2f46bf1ad0
taewan625 <taewan1997625@gmail.com>
2025-08-10 21:41:33 +0900

    #8.9 MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { motion, useMotionValue, useTransform } from "motion/react";
+import { motion, useMotionValue, useScroll, useTransform } from "motion/react";
 import { useEffect } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, rgb(72, 72, 72), rgb(8, 1, 8));
+  height: 200vh;
 `;
 
 const Box = styled(motion.div)`
@@ -21,12 +23,35 @@ const Box = styled(motion.div)`
 function App() {
   const x = useMotionValue(0);
 
-  //측정 값을 가지고 원하는 출력값 출력 방법
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(74, 166, 64), rgb(37, 43, 40))",
+      "linear-gradient(135deg, rgb(72, 72, 72), rgb(8, 1, 8))",
+      "linear-gradient(135deg, rgb(82, 55, 138), rgb(82, 4, 237))",
+    ]
+  );
+
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
+  useEffect(() => {
+    const unsubscribe = scrollYProgress.on("change", () => {
+      console.log(scrollYProgress.get());
+    });
+
+    //useEffect만 사용한다고 해제되는거 아니였음. 이렇게 return 내부에 해제 함수를 작성해야 언마운트시 해제를 함.
+    return () => {
+      console.log("해제");
+      unsubscribe();
+    };
+  }, [scrollYProgress]);
 
   return (
-    <Wrapper>
-      <Box style={{ x, scale: scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag dragSnapToOrigin />
     </Wrapper>
   );
 }
