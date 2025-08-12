0a2343d695ebfed129f5b751a2c7edf4f624a597
taewan625 <taewan1997625@gmail.com>
2025-08-12 23:08:21 +0900

    #9.1 Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
+
 function App() {
-  return null;
+  return (
+    //router 환경 제공
+    <BrowserRouter>
+      <Header />
+      {/* 첫번째 match된 router만 렌더링 */}
+      <Switch>
+        {/* 각 경로 */}
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
