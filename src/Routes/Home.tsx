85bfdbc9992960b4986dba951fbc272e9cd75c35
taewan625 <taewan1997625@gmail.com>
2025-08-16 13:45:05 +0900

    #9.7 Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background-color: #000000;
@@ -29,10 +31,43 @@ const Banner = styled.div<{ bgPhoto: string }>`
 const Title = styled.h2`
   font-size: 68px;
 `;
+
 const OverView = styled.p`
   font-size: 30px;
   width: 50%;
 `;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 33px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.innerWidth - 10,
+  },
+};
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>({
@@ -40,16 +75,38 @@ function Home() {
     queryFn: getMovies,
   });
 
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => 1 + prev);
+
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>loading..</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <OverView>{data?.results[0].overview}</OverView>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ ease: "linear", duration: 1 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
